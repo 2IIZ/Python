913efbfbdbe9380c0166bdfc521d7aa585ba4168
Unknown <2IIZ1998@gmail.com>
2019-01-17 13:19:57 +0100

    update

--- 3_modules/exec_modules.py
+++ 3_modules/exec_modules.py
@@ -1,6 +1,9 @@
-from modules import multiplication
-from modules import sum
+from mypackage.math.modules import multiplication
+from mypackage.math.modules import sum
 #from modules import * -> means all
+from mypackage.helloworld import helloworld
+
+helloworld()
 
 print("Multiplication : ", multiplication(4, 2))
 
--- 3_modules/mypackage/math/modules.py
+++ 3_modules/mypackage/math/modules.py
@@ -3,3 +3,8 @@ def multiplication(number1, number2):
 
 def sum(number1, number2):
     return number1 + number2
+
+
+# testing the function inside my modules
+if __name__ == "__main__":
+    print(sum(4, 2))
