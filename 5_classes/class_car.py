612427b00b68fa162da896027aa8aa2fdd77896d
Unknown <2IIZ1998@gmail.com>
2019-01-21 15:19:12 +0100

    classes

--- 5_classes/class_car.py
+++ 5_classes/class_car.py
@@ -3,3 +3,16 @@ class Car:
     def __init__(self, color):
 
         self.color = color
+
+    # getters
+
+    def _get_color(self):
+        print("The car's color is :", self.color)
+
+    def _set_color(self, color):
+        self.color = color
+        print("Seems that the car's color is changed, now is :", self.color)
+
+
+    # method spécial
+    # https://openclassrooms.com/fr/courses/235344-apprenez-a-programmer-en-python/233046-les-methodes-speciales
--- 5_classes/script.py
+++ 5_classes/script.py
@@ -1,5 +1,9 @@
 from class_car import Car
 
-object_car = Car("red")
+object_car = Car("Bed")
+
+print(object_car.color)
+
+object_car.color = "Blue" # -> access to the _set_color() method
 
 print(object_car.color)
