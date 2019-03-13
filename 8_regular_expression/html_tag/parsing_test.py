fec36261298fcddafd67ab12e7582df817deee2c
Unknown <2IIZ1998@gmail.com>
2019-03-13 18:53:31 +0100

    Added commentaries

--- 8_regular_expression/html_tag/parsing_test.py
+++ 8_regular_expression/html_tag/parsing_test.py
@@ -1,12 +1,14 @@
-import re  # module re
+import re  # module re (Regular expression)
 
-file_object  = open("erable.html", "r", encoding="utf8")
+file_object  = open("erable.html", "r", encoding="utf8") # open file (html from wikipedia)
 
-html = file_object.read()
+html = file_object.read() # read the file and save it to a variable
 
 #___________________________________________________________________
-title_pattern = re.compile("<TITLE>(.+?)</TITLE>")
+# will save the regular expression in a variable
+title_pattern = re.compile("<TITLE>(.+?)</TITLE>") # this specific regex find the title of the wikipedia page
+
 # find the pattern then print what's found.
 title = re.findall(title_pattern, html) # findall() takes all results
-print(title, "\n")
+print(title, "\n") # print 
 #___________________________________________________________________
